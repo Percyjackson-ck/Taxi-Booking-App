d4928857f8ad54e12a5e79713f193982eff4c493
Percyjackson-ck <girishck04@gmail.com>
2025-06-06 17:17:34 +0530

    did some part of home component

---
+++ Frontend/src/Components/LocationSearchPanel.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const LocationSearchPanel = () => {
+  return (
+    <div>
+   {/* this is just sample Data */}
+    <div className='flex items-center my-4 justify-start'>
+        <h2 className='bg-[#eee] flex items-center justify-center h-10 w-10 mr-3   rounded-full'><i className="ri-map-pin-fill"></i></h2>
+        <h4 className='font-medium'>24B,Near kappor's cafe,Sheryains Coding School,Bhipol</h4>
+    </div>
+    <div className='flex items-center my-4 justify-start'>
+        <h2 className='bg-[#eee] flex items-center justify-center h-10 w-10 mr-3   rounded-full'><i className="ri-map-pin-fill"></i></h2>
+        <h4 className='font-medium'>24B,Near kappor's cafe,Sheryains Coding School,Bhipol</h4>
+    </div>
+    <div className='flex items-center my-4 justify-start'>
+        <h2 className='bg-[#eee] flex items-center justify-center h-10 w-10 mr-3   rounded-full'><i className="ri-map-pin-fill"></i></h2>
+        <h4 className='font-medium'>24B,Near kappor's cafe,Sheryains Coding School,Bhipol</h4>
+    </div>
+    <div className='flex items-center my-4 justify-start'>
+        <h2 className='bg-[#eee] flex items-center justify-center h-10 w-10 mr-3   rounded-full'><i className="ri-map-pin-fill"></i></h2>
+        <h4 className='font-medium'>24B,Near kappor's cafe,Sheryains Coding School,Bhipol</h4>
+    </div>
+    <div className='flex items-center my-4 justify-start'>
+        <h2 className='bg-[#eee] flex items-center justify-center h-10 w-10 mr-3   rounded-full'><i className="ri-map-pin-fill"></i></h2>
+        <h4 className='font-medium'>24B,Near kappor's cafe,Sheryains Coding School,Bhipol</h4>
+    </div>
+    
+    </div>
+  )
+}
+
+export default LocationSearchPanel
